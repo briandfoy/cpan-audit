2591b927a6f9fdc28202d6989f55f32f6804b89a
brian d foy <briandfoy@pobox.com>
2023-03-09 06:59:55 -0500

    Try again on GPG

--- lib/CPAN/Audit.pm
+++ lib/CPAN/Audit.pm
@@ -14,7 +14,7 @@ use CPAN::Audit::Version;
 use CPAN::Audit::Query;
 use CPAN::Audit::DB;
 
-our $VERSION = '20230309.003';
+our $VERSION = '20230309.004';
 
 sub new {
     my( $class, %params ) = @_;
