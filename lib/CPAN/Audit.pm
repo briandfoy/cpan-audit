bdfa1da3a47cab93e117f0b8395e8b819c610711
brian d foy <briandfoy@pobox.com>
2023-06-01 14:44:39 -0400

    Bump version

--- lib/CPAN/Audit.pm
+++ lib/CPAN/Audit.pm
@@ -14,7 +14,7 @@ use CPAN::Audit::Version;
 use CPAN::Audit::Query;
 use CPAN::Audit::DB;
 
-our $VERSION = '20230309.004';
+our $VERSION = '20230601.001';
 
 sub new {
 	my( $class, %params ) = @_;
