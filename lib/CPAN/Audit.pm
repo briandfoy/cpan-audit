bc3739a3e23a377fc3f9891c182ae1882bbce737
brian d foy <briandfoy@pobox.com>
2023-07-09 18:08:56 -0400

    Bump the version of the main module

--- lib/CPAN/Audit.pm
+++ lib/CPAN/Audit.pm
@@ -14,7 +14,7 @@ use CPAN::Audit::Version;
 use CPAN::Audit::Query;
 use CPAN::Audit::DB;
 
-our $VERSION = '20230601.002';
+our $VERSION = '20230709.001';
 
 sub new {
 	my( $class, %params ) = @_;
