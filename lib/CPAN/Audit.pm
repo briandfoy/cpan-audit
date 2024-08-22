efde27019e342f0ca7cd5483e00a324fad0530e2
brian d foy <briandfoy@pobox.com>
2024-08-22 02:15:57 -0400

    Data update for 2024-08-22

--- lib/CPAN/Audit.pm
+++ lib/CPAN/Audit.pm
@@ -14,7 +14,7 @@ use CPAN::Audit::Version;
 use CPAN::Audit::Query;
 use CPAN::Audit::DB;
 
-our $VERSION = '20240731.001';
+our $VERSION = '20240822.001';
 
 sub new {
 	my( $class, %params ) = @_;
