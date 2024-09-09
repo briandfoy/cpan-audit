aab821d91b89d81f1005c3f8586b5938efa01893
brian d foy <briandfoy@pobox.com>
2024-09-08 21:02:14 -0400

    Update for CVE-2024-45321 (App::cpanminus)

--- lib/CPAN/Audit.pm
+++ lib/CPAN/Audit.pm
@@ -14,7 +14,7 @@ use CPAN::Audit::Version;
 use CPAN::Audit::Query;
 use CPAN::Audit::DB;
 
-our $VERSION = '20240826.002';
+our $VERSION = '20240908.001';
 
 sub new {
 	my( $class, %params ) = @_;
