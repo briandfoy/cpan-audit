a50fb61779fc36cd286cfd6be3619e8abe9fd17e
brian d foy <briandfoy@pobox.com>
2026-06-22 14:11:52 -0400

    Remove extra stuff from the DBD::Pg qv file

--- t/data/installed/perl5/lib/perl5/DBD/Pg.pm
+++ t/data/installed/perl5/lib/perl5/DBD/Pg.pm
@@ -1,20 +1,3 @@
-#  -*-cperl-*-
-#
-#  Copyright (c) 2002-2026 Greg Sabino Mullane and others: see the Changes file
-#  Portions Copyright (c) 2002 Jeffrey W. Baker
-#  Portions Copyright (c) 1997-2001 Edmund Mergl
-#  Portions Copyright (c) 1994-1997 Tim Bunce
-#
-#  You may distribute under the terms of either the GNU General Public
-#  License or the Artistic License, as specified in the Perl README file.
-
-
-use strict;
-use warnings;
-use 5.008001;
-
-{
-    package DBD::Pg;
-    use version; our $VERSION = qv('3.20.2');
-}
+package DBD::Pg;
+use version; our $VERSION = qv('3.20.2');
 1;
